daf4f5e7543801a20dd0c5e04fa1ac2f70a94d27
Alex Jercan <jercan_alex27@yahoo.com>
2022-11-27 14:26:48 +0200

    feat: added pointnet train

--- pointnet/pointnet.py
+++ pointnet/pointnet.py
@@ -1,3 +1,5 @@
+import torch
+
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -126,7 +128,7 @@ def sample_and_group(npoint, radius, nsample, xyz, points, returnfps=False):
 
 
 class PointNetSetAbstraction(nn.Module):
-    def __init__(self, npoint, radius, nsample, in_channel, mlp, group_all):
+    def __init__(self, npoint, radius, nsample, in_channel, mlp, group_all=False):
         super(PointNetSetAbstraction, self).__init__()
         self.npoint = npoint
         self.radius = radius
@@ -283,17 +285,17 @@ class PointNetFeaturePropagation(nn.Module):
 
 
 
-class get_model(nn.Module):
+class PointNet(nn.Module):
     def __init__(self, num_classes):
-        super(get_model, self).__init__()
-        self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 9 + 3, [32, 32, 64], False)
-        self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 64 + 3, [64, 64, 128], False)
-        self.sa3 = PointNetSetAbstraction(64, 0.4, 32, 128 + 3, [128, 128, 256], False)
-        self.sa4 = PointNetSetAbstraction(16, 0.8, 32, 256 + 3, [256, 256, 512], False)
-        self.fp4 = PointNetFeaturePropagation(768, [256, 256])
-        self.fp3 = PointNetFeaturePropagation(384, [256, 256])
-        self.fp2 = PointNetFeaturePropagation(320, [256, 128])
-        self.fp1 = PointNetFeaturePropagation(128, [128, 128, 128])
+        super().__init__()
+        self.sa1 = PointNetSetAbstraction(npoint=1024, radius=1000, nsample=32, in_channel=7 + 3, mlp=[32, 32, 64])
+        self.sa2 = PointNetSetAbstraction(npoint=256, radius=2000, nsample=32, in_channel=64 + 3, mlp=[64, 64, 128])
+        self.sa3 = PointNetSetAbstraction(npoint=64, radius=4000, nsample=32, in_channel=128 + 3, mlp=[128, 128, 256])
+        self.sa4 = PointNetSetAbstraction(npoint=16, radius=8000, nsample=32, in_channel=256 + 3, mlp=[256, 256, 512])
+        self.fp4 = PointNetFeaturePropagation(in_channel=768, mlp=[256, 256])
+        self.fp3 = PointNetFeaturePropagation(in_channel=384, mlp=[256, 256])
+        self.fp2 = PointNetFeaturePropagation(in_channel=320, mlp=[256, 128])
+        self.fp1 = PointNetFeaturePropagation(in_channel=128, mlp=[128, 128, 128])
         self.conv1 = nn.Conv1d(128, 128, 1)
         self.bn1 = nn.BatchNorm1d(128)
         self.drop1 = nn.Dropout(0.5)
@@ -315,22 +317,10 @@ class get_model(nn.Module):
 
         x = self.drop1(F.relu(self.bn1(self.conv1(l0_points))))
         x = self.conv2(x)
-        x = F.log_softmax(x, dim=1)
-        x = x.permute(0, 2, 1)
         return x, l4_points
 
-
-class get_loss(nn.Module):
-    def __init__(self):
-        super(get_loss, self).__init__()
-    def forward(self, pred, target, trans_feat, weight):
-        total_loss = F.nll_loss(pred, target, weight=weight)
-
-        return total_loss
-
 if __name__ == '__main__':
-    import  torch
-    model = get_model(13)
-    xyz = torch.rand(6, 9, 2048)
+    model = PointNet(10)
+    xyz = torch.rand(6, 7, 2048)
     print(model(xyz))
 
